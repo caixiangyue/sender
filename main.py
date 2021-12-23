86665e1ed9658ff41c84481a1bb628210758c76f
caixiangyue <caixiangyue007@gmail.com>
2021-12-23 19:30:40 +0800

    test

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ PORT = 465
 
 PASSCODE = os.environ['PASS_KEY']
 SENDER = os.environ['SENDER']
-to = SENDER
+TO = os.environ['TO']
 
 
-def send_email():
-    msg = MIMEText('为了您的健康,起来活动活动,喝点儿水!!!啦啦啦啦啦啦啦啦啦.....此程序每隔一个小时会自动给您发送一封邮件','plain','utf-8')
-    msg['subject'] = '来自专属python程序的亲切问候^_^'
-    msg['from'] = formataddr(['发件人昵称',SENDER])
-    msg['to']   = formataddr(['收件人昵称',to])
-    server = smtplib.SMTP_SSL(SERVER, PORT)
+def send_email(server, to):
     # server.set_debuglevel(1)
-    server.login(SENDER, PASSCODE)
-    server.sendmail(SENDER,[SENDER, to], msg.as_string())
-    server.quit()
+    msg = MIMEText('这是一个测试!!!','plain','utf-8')
+    msg['subject'] = '来自 github action'
+    msg['from'] = formataddr(['action',SENDER])
+    msg['to']   = formataddr(['收件人昵称', to])
+    server.sendmail(SENDER, [to], msg.as_string())
+    
 
 if __name__ == "__main__":
-    send_email()
+    server = smtplib.SMTP_SSL(SERVER, PORT)
+    server.login(SENDER, PASSCODE)
+    send_email(server, SENDER)
+    send_email(server, TO)
+    server.quit()
