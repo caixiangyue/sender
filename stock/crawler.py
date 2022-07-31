c4ef781f581fac0dc735585db05f86d20fb46833
caixiangyue <caixiangyue007@gmail.com>
2022-07-31 14:11:54 +0800

    update

--- stock/crawler.py
+++ stock/crawler.py
@@ -29,9 +29,34 @@ class Crawler:
         dom = etree.HTML(r.content)
         data = dom.xpath('//*[@id="data-description"]/text()')
         if len(data) > 0:
-            ret += data[0]
-        ret += '\n'
+            gnp = data[0]
+        ret += f'{gnp}\n'
+        ret += self.get_suggest(gnp)
+        ret += '-----------------------\n\n'
         return ret
+    
+    def get_suggest(self, gnp: str) -> str:
+        start = gnp.find('：')
+        end = gnp.find('%')
+
+        if start == -1 or end == -1:
+            return ''
+        
+        gnp = float(gnp[start+1:end])
+        if gnp > 80.0:
+            return '此时不建议买入任何股票，买了就是大傻瓜\n'
+        
+        if gnp < 65.0:
+            return '此时机会千载难逢\n'
+        
+        if gnp < 70.0:
+            return '此时不买更待何时，绝佳买点，不买就是傻子\n'
+        
+        if gnp < 75.0:
+            return '此时适合买入\n'
+        
+        if gnp < 80.0:
+            return '此时适合定投，少量买入\n'
 
 
 # c = Crawler()
