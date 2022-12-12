38799f74c15580cd92b9c705a3087f11e5465558
caixiangyue <caixiangyue007@gmail.com>
2022-12-12 18:02:14 +0800

    update

--- stock/crawler.py
+++ stock/crawler.py
@@ -3,9 +3,10 @@ import time
 from lxml import etree
 
 from common.utils import HEADERS
+HEADERS1 = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "Referer":"https://stock.finance.sina.com.cn/forex/globalbd/gcny10.html"}
 
 URL = 'https://legulegu.com/stockdata/marketcap-gdp'
-URL1 = 'https://wallstreetcn.com/markets/codes/CN10YR.OTC'
+URL1 = 'https://hq.sinajs.cn/?rn=1670838672882&list=globalbd_gcny10'
 
 class Crawler:
     def __init__(self) -> None:
@@ -41,7 +42,7 @@ class Crawler:
         retry_times = 10
         while retry_times > 0:
             try:
-                r = requests.get(URL1, headers=HEADERS)
+                r = requests.get(URL1, headers=HEADERS1)
                 if r.status_code != 200:
                     retry_times -= 1
                     continue
@@ -54,40 +55,40 @@ class Crawler:
                 time.sleep(1)
 
         ret = '十年期国债：\n'
-        dom = etree.HTML(r.content)
-        data = dom.xpath('//*[@id="app"]/div/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/div[1]/div[2]/div[1]/text()')
+
+        data = r.content.decode('gbk').split(',')
         ten_rate=''
-        if len(data) > 0:
-            ten_rate = data[0].strip('\n').strip('\r').strip(' ')
+        if len(data) > 3:
+            ten_rate = data[3]
         ret += f'{ten_rate}\n'
         if len(ten_rate) > 0 and float(ten_rate) < 3.0:
             ret += '此时债券及其没有性价比，不建议买入\n'
         ret += '-----------------------\n\n'
         return ret
-    
+
     def get_suggest(self, gnp: str) -> str:
         start = gnp.find('：')
         end = gnp.find('%')
 
         if start == -1 or end == -1:
             return ''
-        
+
         gnp = float(gnp[start+1:end])
         if gnp > 80.0:
             return '此时不建议买入任何股票，买了就是大傻瓜\n'
-        
+
         if gnp < 65.0:
             return '此时机会千载难逢\n'
-        
+
         if gnp < 70.0:
             return '此时不买更待何时，绝佳买点，不买就是傻子\n'
-        
+
         if gnp < 75.0:
             return '此时适合买入\n'
-        
+
         if gnp < 80.0:
             return '此时适合定投，少量买入\n'
 
 
-#c = Crawler()
-#print(c.get_ten_years())
+# c = Crawler()
+# print(c.get_ten_years())
