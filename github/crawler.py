1db5e3af256ca2e1915a9119fce51555c234f50b
caixiangyue <caixiangyue007@gmail.com>
2023-04-17 10:46:15 +0800

    fix github

--- github/crawler.py
+++ github/crawler.py
@@ -3,7 +3,7 @@ import time
 from lxml import etree
 
 from common.utils import HEADERS
-
+# HEADERS = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}
 URL = "https://github.com/trending"
 
 class Crawler:
@@ -28,29 +28,30 @@ class Crawler:
         ret = '获取 github trending 。。。\n'
         dom = etree.HTML(r.content)
         div = dom.xpath('//div[@class="Box"]/div[2]')
-        for i in range(20):
-            text_list = div[0].xpath(f'//article[{i+1}]/h1/a/text()')
-            href_list = div[0].xpath(f'//article[{i+1}]/h1/a/@href')
+        for i in range(25):
+            text_list = div[0].xpath(f'//article[{i+1}]/h2/a/text()')
+
+            href_list = div[0].xpath(f'//article[{i+1}]/h2/a/@href')
             if len(text_list) == 0 or len(href_list) == 0:
                 break
 
             p_list = div[0].xpath(f'//article[{i+1}]/p/text()')
             href = f'https://github.com{href_list[0]}'
-            
+
             text = ''
             for t in text_list:
                 text += t.strip().replace('\n', '')
             desc = ''
             for p in p_list:
                 desc += p
-            
+
             ret += f'{i+1} {text}\n'
             ret += f'{desc}\n'
             ret += f'{href}\n'
         ret += '\n'
         return ret
 
-            
+
 
 # c = Crawler()
 # print(c.get_trending_msg())
